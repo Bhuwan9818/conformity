bc30250559f588340724ae55a70556c7e872a15a
bhuwan <bhuwansingh9818@gmil.com>
2025-03-20 00:11:31 +0530

    new changes

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -10,13 +10,3 @@ document.addEventListener("DOMContentLoaded", function () {
         megaMenu.style.display = "none";
     });
 });
-
-document.addEventListener("DOMContentLoaded", function () {
-    let icon = document.querySelector(".icon");
-    let menuHidden = document.querySelector(".collapse");
-
-    icon.addEventListener("click", function () {
-        menuHidden.classList.toggle("showData");
-        console.log(menuHidden);
-    });
-});
