0122c13acc43c94549b4322718680e059b2756ad
bhuwan <bhuwansingh9818@gmil.com>
2025-03-23 22:43:02 +0530

    new changes

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,11 +1,11 @@
 
 // Hover Animation with GSAP
-document.querySelectorAll(".sidebar ul li").forEach(item => {
-    item.addEventListener("mouseenter", () => {
-        gsap.to(item, { scale: 1.07, duration: 0.5 });
-    });
+// document.querySelectorAll(".sidebar ul li").forEach(item => {
+//     item.addEventListener("mouseenter", () => {
+//         gsap.to(item, { scale: 1.07, duration: 0.5 });
+//     });
 
-    item.addEventListener("mouseleave", () => {
-        gsap.to(item, { scale: 1, duration: 0.2 });
-    });
-});
+//     item.addEventListener("mouseleave", () => {
+//         gsap.to(item, { scale: 1, duration: 0.2 });
+//     });
+// });
