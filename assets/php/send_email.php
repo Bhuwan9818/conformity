d36512f9cd2991030e71065b4f92ce2c4cd0323e
bhuwan <bhuwansingh9818@gmail.com>
2025-05-24 03:26:49 -0400

    new changes

--- assets/php/send_email.php
+++ assets/php/send_email.php
@@ -7,7 +7,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $company = htmlspecialchars($_POST['company']);
     $message = htmlspecialchars($_POST['message']);
 
-    $to = "bhuwansingh9818@gmail.com"; // Replace with your Gmail address
+    $to = "bhuwansingh886043@gmail.com"; // Replace with your Gmail address
     $subject = "New Contact Form Submission";
     $body = "Name: $name\nEmail: $email\nphone: $phone\nservice: $service\ncompany: $company\nMessage:\n$message";
     $headers = "From: $email";
