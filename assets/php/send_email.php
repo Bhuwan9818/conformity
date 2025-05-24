49ea2ea1f494185d69917f0e25add70817f8857d
bhuwan <bhuwansingh9818@gmail.com>
2025-05-24 04:00:41 -0400

    new changes

--- assets/php/send_email.php
+++ assets/php/send_email.php
@@ -9,9 +9,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $to = "bhuwansingh886043@gmail.com"; // Replace with your Gmail address
     $subject = "New Contact Form Submission";
-    $body = "Name: $name\nEmail: $email\nphone: $phone\nservice: $service\ncompany: $company\nMessage:\n$message";
+    $body = "Name: $name\nEmail: $email\nphone: $phone\nservice: $service\ncompany: $company\nMessage:$message";
     $headers = "From: pankaj.k@conformityalliance.in";
-    // $headers .= "reply-To: $email\r\n";
+    $headers .= "reply-To: $email\r\n";
     // $headers .= "X-Mailer: PHP/".phpversion();
 
     if (mail($to, $subject, $body, $headers)) {
